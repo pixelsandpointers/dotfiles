d6c4c6f8681c05c34bad57734c7704be1e297a51
Benjamin Beilharz <b@ben.graphics>
2024-05-16 11:11:16 +0200

    update wsl config

---
+++ custom-emacs/elpa/org-modern-20240430.2112/org-modern-pkg.el
@@ -0,0 +1,15 @@
+(define-package "org-modern" "20240430.2112" "Modern looks for Org"
+  '((emacs "27.1")
+    (compat "29.1.4.4"))
+  :commit "5393edbb5a078348d0eea5723868c35be3e73494" :authors
+  '(("Daniel Mendler" . "mail@daniel-mendler.de"))
+  :maintainers
+  '(("Daniel Mendler" . "mail@daniel-mendler.de"))
+  :maintainer
+  '("Daniel Mendler" . "mail@daniel-mendler.de")
+  :keywords
+  '("outlines" "hypermedia" "text")
+  :url "https://github.com/minad/org-modern")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
