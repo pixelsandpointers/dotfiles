6c23aaacb9f1be1c4528a1666f9e997952706e53
Benjamin Beilharz <pixelsandpointers@pm.me>
2024-11-28 10:34:37 +0100

    update

--- doom/init.el
+++ doom/init.el
@@ -150,24 +150,24 @@
        ;;idris             ; a language you can depend on
        ;;json              ; At least it ain't XML
        ;;(java +lsp)       ; the poster child for carpal tunnel syndrome
-       ;;javascript        ; all(hope(abandon(ye(who(enter(here))))))
+       javascript        ; all(hope(abandon(ye(who(enter(here))))))
        ;;julia             ; a better, faster MATLAB
        ;;kotlin            ; a better, slicker Java(Script)
        (latex
         +cdlatex
+        +latexmk
         +lsp)    ; writing papers in Emacs has never been so fun
        ;;lean              ; for folks with too much to prove
        ;;ledger            ; be audit you can be
        ;;lua               ; one-based indices? one-based indices
        markdown          ; writing docs for people to ignore
        ;;nim               ; python + lisp at the speed of c
-       ;;nix               ; I hereby declare "nix geht mehr!"
+       nix               ; I hereby declare "nix geht mehr!"
        ;;ocaml             ; an objective camel
        (org
 	+dragndrop
         +gnuplot
         +journal
-        +jupyter
         +noter
         +pandoc
         +roam2
@@ -198,7 +198,7 @@
        ;;solidity          ; do you need a blockchain? No.
        ;;swift             ; who asked for emoji variables?
        ;;terra             ; Earth and Moon in alignment for performance.
-       ;;web               ; the tubes
+       web               ; the tubes
        ;;yaml              ; JSON, but readable
        ;;(zig               ; C, but simpler
        ;;+lsp)
--- nix/flake.nix
+++ nix/flake.nix
@@ -9,111 +9,130 @@
   };
 
   outputs = inputs@{ self, nix-darwin, nixpkgs, nix-homebrew }:
-  let
-    configuration = { pkgs, config, ... }: {
-      # List packages installed in system profile. To search by name, run:
-      # $ nix-env -qaP | grep wget
+    let
+      configuration = { pkgs, config, ... }: let
+        # FIXME: tex xis not recognized as a variable, which is valid because it is not used in in
+        # This will probably need to rewritten, that we pass the combined package into
+        # the environment systemPackages. For now, live without biblatex.
+        # tex = texlive.withPackages (ps: [ps.biblatex ps.collection-bibtexextra]);
+        # tex = pkgs.texlive.combine {
+        #   inherit (pkgs.texlive) scheme-full latexmk biblatex;
+        #};
+      in {
+          # List packages installed in system profile. To search by name, run:
+          # $ nix-env -qaP | grep wget
 
-      # The platform the configuration will be used on.
-      nixpkgs.config.allowUnfree = true;
-      nixpkgs.hostPlatform = "aarch64-darwin";
+          # The platform the configuration will be used on.
+          nixpkgs.config.allowUnfree = true;
+          # nixpkgs.config.allowUnsupportedSystem = true;
+          nixpkgs.hostPlatform = "aarch64-darwin";
 
-      homebrew = {
-        enable = true;
-	taps = [
-	  "FelixKratz/formulae"
-	];
+          homebrew = {
+            enable = true;
+            onActivation.cleanup = "zap";
+	          taps = [
+	            "FelixKratz/formulae"
+	          ];
 
-	brews = [
-	    "borders"
-	];
+	          brews = [
+              "assimp"
+	            "borders"
+	          ];
 
-	casks = [
-	  "nikitabobko/tap/aerospace"
-	  "proton-drive"
-	  "signal"
-      "jetbrains-toolbox"
-	  "zoom"
-	  "blender"
-	];
-	masApps = {
-	};
+	          casks = [
+              "emacs"
+	            "nikitabobko/tap/aerospace"
+	            "proton-drive"
+	            "signal"
+              "jetbrains-toolbox"
+	            "zoom"
+	            "blender"
+              "obsidian"
+	          ];
+	          masApps = {
+	          };
+          };
 
-	onActivation.cleanup = "none";
-      };
 
-      environment.systemPackages = [ 
-	  # System
-      pkgs.alacritty
-      pkgs.oh-my-posh
-	  pkgs.htop
-	  pkgs.imgcat
-	  pkgs.git
-	  pkgs.gh
-	  pkgs.texliveFull
+          environment.systemPackages = [
+	          # System
+		        pkgs.alacritty
+		        pkgs.oh-my-posh
+	          pkgs.htop
+            pkgs.ripgrep
+            pkgs.fd
+            pkgs.tmux
+            pkgs.git
+            pkgs.fh
+            pkgs.gh
 
-	  # Programming
-	  pkgs.cmake
-	  pkgs.llvm
-      pkgs.libllvm
-	  pkgs.rustup
-	  pkgs.python3
-	  pkgs.ruff
-	  pkgs.uv
-	  pkgs.nodejs_22
-	  pkgs.hugo
+	          # Programming
+	          pkgs.cmake
+	          pkgs.llvm
+            pkgs.lldb
+            pkgs.libllvm  # need to manually set LD_LIBRARY_PATH
+	          pkgs.rustup
+	          pkgs.python3
+            pkgs.python312Packages.pip
+	          pkgs.ruff
+	          pkgs.uv
+	          pkgs.nodejs_22
+	          pkgs.hugo
 
-	  # Editors
-	  pkgs.emacs
-	  pkgs.neovim
-	  pkgs.tmux
+	          # Editors
+	          pkgs.neovim
 
-	  # PDF tools
-	  pkgs.pkg-config 
-	  pkgs.poppler 
-	  pkgs.autoconf 
-	  pkgs.automake
+	          # Latex
+            pkgs.texliveFull
+            (pkgs.texlive.withPackages(ps: with ps; [biblatex natbib latexmk bibtools]))
+            # Emacs PDF Tools Dependencies
+	          pkgs.pkg-config
+	          pkgs.poppler
+	          pkgs.autoconf
+	          pkgs.automake
 
-	  # GUI apps
-	  pkgs.mkalias # required for GUI applications to show up in the Finder
-	  pkgs.anki-bin
-	  pkgs.brave
-	  pkgs.element-desktop
-	  pkgs.inkscape
-      pkgs.vscode
-	  pkgs.zotero
+	          # GUI apps, may want to put them into casks for the sake of findability
+	          pkgs.mkalias # required for GUI applications to show up in Finder using aliases
+	          pkgs.anki-bin
+	          pkgs.brave
+            pkgs.discord
+	          pkgs.element-desktop
+	          pkgs.inkscape
+            pkgs.vscode
+	          pkgs.zotero
 
-          # VFX
-	  pkgs.openexr
-	  pkgs.openusd
-	  pkgs.python312Packages.openusd
-      ];
+            # VFX
+            pkgs.assimp
+	          pkgs.openexr
+	          pkgs.openusd
+	          pkgs.python312Packages.openusd
+          ];
 
-      fonts.packages = [
-        (pkgs.nerdfonts.override {fonts = [ "JetBrainsMono" ]; })
-      ];
+          fonts.packages = [
+            (pkgs.nerdfonts.override {fonts = [ "JetBrainsMono" ]; })
+          ];
 
-      services.nix-daemon.enable = true;
+          services.nix-daemon.enable = true;
 
-      # Necessary for using flakes on this system.
-      nix.settings.experimental-features = "nix-command flakes";
+          # Necessary for using flakes on this system.
+          nix.settings.experimental-features = "nix-command flakes";
 
-      # Enable alternative shell support in nix-darwin.
-      # programs.fish.enable = true;
-      programs.zsh.enable = true;
+          # Enable alternative shell support in nix-darwin.
+          # programs.fish.enable = true;
+          programs.zsh.enable = true;
 
-      # Set Git commit hash for darwin-version.
-      system.configurationRevision = self.rev or self.dirtyRev or null;
+          # Set Git commit hash for darwin-version.
+          system.configurationRevision = self.rev or self.dirtyRev or null;
 
-      # set up aliases
-      system.activationScripts.applications.text = let
-	env = pkgs.buildEnv {
-	    name = "system-applications";
-	    paths = config.environment.systemPackages;
-	    pathsToLink = "/Applications";
-	  };
-	in
-	  pkgs.lib.mkForce ''
+          # set up aliases
+          system.activationScripts.applications.text = let
+	          env = pkgs.buildEnv {
+	            name = "system-applications";
+	            paths = config.environment.systemPackages;
+	            pathsToLink = "/Applications";
+	          };
+	        in
+	          pkgs.lib.mkForce ''
 	  # Set up applications.
 	  echo "setting up /Applications..." >&2
 	  rm -rf /Applications/Nix\ Apps
@@ -126,41 +145,41 @@
 	  done
 	      '';
 
-      # Used for backwards compatibility, please read the changelog before changing.
-      # $ darwin-rebuild changelog
-      system.stateVersion = 5;
-      system.defaults = {
-	dock.autohide = true;
-	dock.persistent-apps = [
-	  "${pkgs.brave}/Applications/Brave.app"
-	  "${pkgs.zotero}/Applications/Zotero.app"
-	  "/System/Applications/Terminal.app"
-	];
-	
-	finder.FXPreferredViewStyle = "clmv";
-	NSGlobalDomain.AppleInterfaceStyle = "Dark";
-      };
-    };
-  in
-  {
-    # Build darwin flake using:
-    # $ darwin-rebuild build --flake .#simple
-    darwinConfigurations."zen" = nix-darwin.lib.darwinSystem {
-      modules = [ 
-        configuration 
-	nix-homebrew.darwinModules.nix-homebrew
-	{
-	  nix-homebrew = {
-	    enable = true;
-	    enableRosetta = true;
-	    user = "b";
-	    autoMigrate = true;
-	  };
-	}
-      ];
-    };
+          # Used for backwards compatibility, please read the changelog before changing.
+          # $ darwin-rebuild changelog
+          system.stateVersion = 5;
+          system.defaults = {
+	          dock.autohide = true;
+	          dock.persistent-apps = [
+	            "${pkgs.brave}/Applications/Brave.app"
+	            "${pkgs.zotero}/Applications/Zotero.app"
+	            "/System/Applications/Terminal.app"
+	          ];
 
-    # Expose the package set, including overlays, for convenience.
-    darwinPackages = self.darwinConfigurations."zen".pkgs;
-  };
+	          finder.FXPreferredViewStyle = "clmv";
+	          NSGlobalDomain.AppleInterfaceStyle = "Dark";
+          };
+        };
+    in
+      {
+        # Build darwin flake using:
+        # $ darwin-rebuild build --flake .#simple
+        darwinConfigurations."zen" = nix-darwin.lib.darwinSystem {
+          modules = [
+            configuration
+	          nix-homebrew.darwinModules.nix-homebrew
+	          {
+	            nix-homebrew = {
+	              enable = true;
+	              enableRosetta = true;
+	              user = "b";
+	              autoMigrate = true;
+	            };
+	          }
+          ];
+        };
+
+        # Expose the package set, including overlays, for convenience.
+        darwinPackages = self.darwinConfigurations."zen".pkgs;
+      };
 }
