ddd27f818650c0da42f5f5d75b77546c3eee8d27
Benjamin Beilharz <b@ben.graphics>
2025-02-13 16:06:41 +0100

    Added Latex keymaps

--- doom/custom.el
+++ doom/custom.el
@@ -6,7 +6,8 @@
  ;; If there is more than one, they won't work right.
  '(elfeed-feeds
    '("https://jendrikillner.com/tags/weekly/index.xml"
-     ("https://gpuopen.com/feed/" graphics))))
+     ("https://gpuopen.com/feed/" graphics)))
+ '(package-selected-packages '(nano-agenda nano-theme nano-modeline pet nix-mode)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
