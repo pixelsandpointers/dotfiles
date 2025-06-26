14005c784a5389e227f68e6568627e39035b0645
Benjamin Beilharz <b@ben.graphics>
2025-06-26 16:59:23 +0200

    Nvim: added snacks

---
+++ nvim/lua/plugins/snacks.lua
@@ -0,0 +1,166 @@
+return {
+  'folke/snacks.nvim',
+  priority = 1000,
+  lazy = false,
+  ---@type snacks.Config
+  opts = {
+    bigfile = { enabled = true },
+    dashboard = {
+      enabled = true,
+      sections = {
+        { section = 'header' },
+        {
+          pane = 2,
+          section = 'terminal',
+          cmd = 'colorscript -e square',
+          height = 5,
+          padding = 1,
+        },
+        { section = 'keys', gap = 1, padding = 1 },
+        {
+          pane = 2,
+          icon = ' ',
+          desc = 'Browse Repo',
+          padding = 1,
+          key = 'b',
+          action = function()
+            Snacks.gitbrowse()
+          end,
+        },
+        function()
+          local in_git = Snacks.git.get_root() ~= nil
+          local cmds = {
+            {
+              title = 'Notifications',
+              cmd = 'gh notify -s -a -n5',
+              action = function()
+                vim.ui.open 'https://github.com/notifications'
+              end,
+              key = 'n',
+              icon = ' ',
+              height = 5,
+              enabled = true,
+            },
+            {
+              title = 'Open Issues',
+              cmd = 'gh issue list -L 3',
+              key = 'i',
+              action = function()
+                vim.fn.jobstart('gh issue list --web', { detach = true })
+              end,
+              icon = ' ',
+              height = 7,
+            },
+            {
+              icon = ' ',
+              title = 'Open PRs',
+              cmd = 'gh pr list -L 3',
+              key = 'P',
+              action = function()
+                vim.fn.jobstart('gh pr list --web', { detach = true })
+              end,
+              height = 7,
+            },
+            {
+              icon = ' ',
+              title = 'Git Status',
+              cmd = 'git --no-pager diff --stat -B -M -C',
+              height = 10,
+            },
+          }
+          return vim.tbl_map(function(cmd)
+            return vim.tbl_extend('force', {
+              pane = 2,
+              section = 'terminal',
+              enabled = in_git,
+              padding = 1,
+              ttl = 5 * 60,
+              indent = 3,
+            }, cmd)
+          end, cmds)
+        end,
+        { section = 'startup' },
+      },
+    },
+    -- explorer = { enabled = true },
+    -- indent = { enabled = true },
+    -- input = { enabled = true },
+    -- notifier = {
+    --   enabled = true,
+    --   timeout = 3000,
+    -- },
+    -- picker = { enabled = true },
+    -- quickfile = { enabled = true },
+    -- scope = { enabled = true },
+    -- scroll = { enabled = true },
+    statuscolumn = { enabled = true },
+    words = { enabled = true },
+    styles = {
+      notification = {
+        -- wo = { wrap = true } -- Wrap notifications
+      },
+    },
+  },
+  keys = {
+    {
+      '<leader>fP',
+      function()
+        Snacks.picker.projects()
+      end,
+      desc = 'Projects',
+    },
+    {
+      '<leader>fl',
+      function()
+        Snacks.picker.lines()
+      end,
+      desc = 'Buffer Lines',
+    },
+    {
+      '<leader>N',
+      desc = 'Neovim News',
+      function()
+        Snacks.win {
+          file = vim.api.nvim_get_runtime_file('doc/news.txt', false)[1],
+          width = 0.6,
+          height = 0.6,
+          wo = {
+            spell = false,
+            wrap = false,
+            signcolumn = 'yes',
+            statuscolumn = ' ',
+            conceallevel = 3,
+          },
+        }
+      end,
+    },
+  },
+  init = function()
+    vim.api.nvim_create_autocmd('User', {
+      pattern = 'VeryLazy',
+      callback = function()
+        -- Setup some globals for debugging (lazy-loaded)
+        _G.dd = function(...)
+          Snacks.debug.inspect(...)
+        end
+        _G.bt = function()
+          Snacks.debug.backtrace()
+        end
+        vim.print = _G.dd -- Override print to use snacks for `:=` command
+
+        -- Create some toggle mappings
+        Snacks.toggle.option('spell', { name = 'Spelling' }):map '<leader>us'
+        Snacks.toggle.option('wrap', { name = 'Wrap' }):map '<leader>uw'
+        Snacks.toggle.option('relativenumber', { name = 'Relative Number' }):map '<leader>uL'
+        Snacks.toggle.diagnostics():map '<leader>ud'
+        Snacks.toggle.line_number():map '<leader>ul'
+        Snacks.toggle.option('conceallevel', { off = 0, on = vim.o.conceallevel > 0 and vim.o.conceallevel or 2 }):map '<leader>uc'
+        Snacks.toggle.treesitter():map '<leader>uT'
+        Snacks.toggle.option('background', { off = 'light', on = 'dark', name = 'Dark Background' }):map '<leader>ub'
+        Snacks.toggle.inlay_hints():map '<leader>uh'
+        Snacks.toggle.indent():map '<leader>ug'
+        Snacks.toggle.dim():map '<leader>uD'
+      end,
+    })
+  end,
+}
