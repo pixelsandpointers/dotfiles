6d7538771795687addea30a96501ca2052096ee6
Benjamin Beilharz <b@ben.graphics>
2025-06-26 18:00:06 +0200

    Nvim: added keybindings for session

--- nvim/lua/plugins/session.lua
+++ nvim/lua/plugins/session.lua
@@ -3,5 +3,7 @@ return {
   version = false,
   config = function()
     require('mini.sessions').setup {}
+    local map = require('utils').map
+    map('n', '<leader>qs', '<cmd>mksession<CR>', { noremap = true, desc = 'Save session' })
   end,
 }
