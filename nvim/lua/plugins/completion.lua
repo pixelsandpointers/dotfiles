760294052caf866abe142002ce6e3c63fff603a6
Benjamin Beilharz <b@ben.graphics>
2025-07-22 12:07:34 +0200

    Nvim: Replaced nvim-cmp with blink-cmp

--- nvim/lua/plugins/completion.lua
+++ nvim/lua/plugins/completion.lua
@@ -1,89 +1,97 @@
 return {
-  -- auto completion
   {
-    'hrsh7th/nvim-cmp',
-    version = false, -- last release is way too old
-    event = 'InsertEnter',
+    'saghen/blink.cmp',
     dependencies = {
-      'hrsh7th/cmp-nvim-lsp',
-      'hrsh7th/cmp-buffer',
-      'hrsh7th/cmp-path',
+      { 'rafamadriz/friendly-snippets' },
+      { 'L3MON4D3/LuaSnip', version = 'v2.*' },
     },
-    opts = function()
-      vim.api.nvim_set_hl(0, 'CmpGhostText', { link = 'Comment', default = true })
-      local luasnip = require 'luasnip'
-      local cmp = require 'cmp'
-      local defaults = require 'cmp.config.default'()
-      local has_words_before = function()
-        unpack = unpack or table.unpack
-        local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-        return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match '%s' == nil
-      end
-      return {
-        completion = {
-          completeopt = 'menu,menuone,noselect',
-        },
-        mapping = cmp.mapping.preset.insert {
-          ['<Tab>'] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-              cmp.select_next_item()
-              -- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
-              -- this way you will only jump inside the snippet region
-            elseif luasnip.expand_or_locally_jumpable() then
-              luasnip.expand_or_jump()
-            elseif has_words_before() then
-              cmp.complete()
-            else
-              fallback()
+
+    version = '1.*',
+
+    ---@module 'blink.cmp'
+    ---@type blink.cmp.Config
+    opts = {
+      keymap = {
+        preset = 'enter',
+        ['<Tab>'] = {
+          function(cmp)
+            -- check if completion window is open
+            if cmp.is_active() then
+              return cmp.select_next()
             end
-          end, { 'i', 's' }),
-          ['<S-Tab>'] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-              cmp.select_prev_item()
-            elseif luasnip.jumpable(-1) then
-              luasnip.jump(-1)
-            else
-              fallback()
-            end
-          end, { 'i', 's' }),
-          ['<C-n>'] = cmp.mapping.select_next_item { behavior = cmp.SelectBehavior.Insert },
-          ['<C-p>'] = cmp.mapping.select_prev_item { behavior = cmp.SelectBehavior.Insert },
-          ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-          ['<C-f>'] = cmp.mapping.scroll_docs(4),
-          ['<C-Space>'] = cmp.mapping.complete(),
-          ['<C-e>'] = cmp.mapping.abort(),
-          -- NOTE: Make enter jump to next placeholder to do not complete snippets
-          ['<CR>'] = cmp.mapping.confirm { select = false }, -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-          ['<S-CR>'] = cmp.mapping.confirm {
-            behavior = cmp.ConfirmBehavior.Replace,
-            select = true,
-          }, -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-          ['<C-CR>'] = function(fallback)
-            cmp.abort()
-            fallback()
           end,
+          'snippet_forward',
+          'fallback',
         },
-        sources = cmp.config.sources({
-          { name = 'nvim_lsp' },
-          { name = 'path' },
-        }, {
-          { name = 'buffer' },
-        }),
-        experimental = {
-          ghost_text = {
-            hl_group = 'CmpGhostText',
+        ['<S-Tab>'] = {
+          function(cmp)
+            -- check if completion window is open
+            if cmp.is_active() then
+              return cmp.select_prev()
+            end
+          end,
+          'snippet_backward',
+          'fallback',
+        },
+        ['<C-e>'] = { 'hide', 'fallback' },
+      },
+
+      cmdline = {
+        enabled = true,
+      },
+      appearance = {
+        -- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
+        -- Adjusts spacing to ensure icons are aligned
+        nerd_font_variant = 'mono',
+      },
+
+      completion = {
+        trigger = {
+          show_on_insert_on_trigger_character = false,
+        },
+        accept = {
+          auto_brackets = {
+            enabled = true,
           },
         },
-        sorting = defaults.sorting,
-      }
-    end,
-    ---@param opts cmp.ConfigSchema
-    config = function(_, opts)
-      for _, source in ipairs(opts.sources) do
-        source.group_index = source.group_index or 1
-      end
-      require('cmp').setup(opts)
-    end,
+        documentation = {
+          auto_show = true,
+          auto_show_delay_ms = 500,
+        },
+        ghost_text = {
+          enabled = true,
+        },
+        keyword = {
+          range = 'full',
+        },
+        list = {
+          selection = {
+            preselect = false,
+            auto_insert = false,
+          },
+        },
+      },
+
+      sources = {
+        default = { 'lsp', 'path', 'snippets', 'buffer' },
+      },
+
+      snippets = {
+        preset = 'luasnip',
+      },
+
+      signature = {
+        enabled = true,
+      },
+
+      -- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
+      -- You may use a lua implementation instead by using `implementation = "lua"` or fallback to the lua implementation,
+      -- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
+      --
+      -- See the fuzzy documentation for more information
+      fuzzy = { implementation = 'prefer_rust_with_warning' },
+    },
+    opts_extend = { 'sources.default' },
   },
 
   -- snippets
@@ -97,41 +105,27 @@ return {
           require('luasnip.loaders.from_vscode').lazy_load()
         end,
       },
-      {
-        'nvim-cmp',
-        dependencies = {
-          'saadparwaiz1/cmp_luasnip',
-        },
-        opts = function(_, opts)
-          opts.snippet = {
-            expand = function(args)
-              require('luasnip').lsp_expand(args.body)
-            end,
-          }
-          table.insert(opts.sources, { name = 'luasnip' })
-        end,
+      opts = {
+        history = true,
+        delete_check_events = 'TextChanged',
       },
-    },
-    opts = {
-      history = true,
-      delete_check_events = 'TextChanged',
-    },
     -- stylua: ignore
     keys = function()
     return {}
     end,
-  },
+    },
 
-  -- INFO: Gilles like snippets for LuaSnip
-  {
-    'iurimateus/luasnip-latex-snippets.nvim',
-    -- vimtex isn't required if using treesitter
-    requires = { 'L3MON4D3/LuaSnip', 'lervag/vimtex' },
-    config = function()
-      require('luasnip-latex-snippets').setup {
-        use_treesitter = true,
-      }
-      require('luasnip').config.setup { enable_autosnippets = true }
-    end,
+    -- INFO: Gilles like snippets for LuaSnip
+    {
+      'iurimateus/luasnip-latex-snippets.nvim',
+      -- vimtex isn't required if using treesitter
+      requires = { 'L3MON4D3/LuaSnip', 'lervag/vimtex' },
+      config = function()
+        require('luasnip-latex-snippets').setup {
+          use_treesitter = true,
+        }
+        require('luasnip').config.setup { enable_autosnippets = true }
+      end,
+    },
   },
 }
