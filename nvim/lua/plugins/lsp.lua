8a553a4160bd1dd3764fec1f561207638a9dc412
Benjamin Beilharz <b@ben.graphics>
2025-08-04 11:57:39 +0200

    Nvim: remove telescope LSP bindings

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -61,50 +61,6 @@ return { -- LSP Configuration & Plugins
     vim.api.nvim_create_autocmd('LspAttach', {
       group = vim.api.nvim_create_augroup('kickstart-lsp-attach', { clear = true }),
       callback = function(event)
-        local map = function(keys, func, desc)
-          vim.keymap.set('n', keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc })
-        end
-
-        -- Jump to the definition of the word under your cursor.
-        --  This is where a variable was first declared, or where a function is defined, etc.
-        --  To jump back, press <C-T>.
-        map('gd', require('telescope.builtin').lsp_definitions, 'Goto Definition')
-
-        -- Find references for the word under your cursor.
-        map('gr', require('telescope.builtin').lsp_references, 'Goto References')
-
-        -- Jump to the implementation of the word under your cursor.
-        --  Useful when your language has ways of declaring types without an actual implementation.
-        map('gI', require('telescope.builtin').lsp_implementations, 'Goto Implementation')
-
-        -- Fuzzy find all the symbols in your current document.
-        --  Symbols are things like variables, functions, types, etc.
-        map('<leader>cs', require('telescope.builtin').lsp_document_symbols, 'Document Symbols')
-
-        -- Fuzzy find all the symbols in your current workspace
-        --  Similar to document symbols, except searches over your whole project.
-        map('<leader>cws', require('telescope.builtin').lsp_dynamic_workspace_symbols, 'Workspace Symbols')
-
-        -- Rename the variable under your cursor
-        --  Most Language Servers support renaming across files, etc.
-        map('<leader>cr', vim.lsp.buf.rename, 'Rename')
-
-        -- Execute a code action, usually your cursor needs to be on top of an error
-        -- or a suggestion from your LSP for this to activate.
-        map('<leader>ca', vim.lsp.buf.code_action, 'Code Action')
-
-        -- Opens a popup that displays documentation about the word under your cursor
-        --  See `:help K` for why this keymap
-        map('K', vim.lsp.buf.hover, 'Hover Documentation')
-
-        -- WARN: This is not Goto Definition, this is Goto Declaration.
-        --  For example, in C this would take you to the header
-        map('gD', vim.lsp.buf.declaration, 'Goto Declaration')
-
-        -- The following two autocommands are used to highlight references of the
-        -- word under your cursor when your cursor rests there for a little while.
-        --    See `:help CursorHold` for information about when this is executed
-        --
         -- When you move your cursor, the highlights will be cleared (the second autocommand).
         local client = vim.lsp.get_client_by_id(event.data.client_id)
         if client and client.server_capabilities.documentHighlightProvider then
@@ -141,13 +97,14 @@ return { -- LSP Configuration & Plugins
 
     -- manual LSP config (cannot be installed by Mason)
     lspconfig['slangd'].setup { capabilities = capabilities }
-    local sourcekit_capabilities = vim.tbl_deep_extend('force', native_capabilities, {
-      workspace = {
-        didChangeWatchedFiles = {
-          dynamicRegistration = true,
-        },
-      },
-    })
-    lspconfig['sourcekit'].setup { capabilities = sourcekit_capabilities, filetypes = 'swift' }
+    -- local sourcekit_capabilities = vim.tbl_deep_extend('force', native_capabilities, {
+    --   filetypes = 'swift',
+    --   workspace = {
+    --     didChangeWatchedFiles = {
+    --       dynamicRegistration = true,
+    --     },
+    --   },
+    -- })
+    -- lspconfig['sourcekit'].setup { capabilities = sourcekit_capabilities }
   end,
 }
