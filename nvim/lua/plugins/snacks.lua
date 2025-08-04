eea86323cd297def3afcc01cfa36d0c87c516bbc
Benjamin Beilharz <b@ben.graphics>
2025-08-04 11:58:00 +0200

    Nvim: add snacks.picker

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -1,3 +1,459 @@
+keymaps = {
+  -- Top Pickers & Explorer
+  -- {
+  --   '<leader><space>',
+  --   function()
+  --     Snacks.picker.smart()
+  --   end,
+  --   desc = 'Smart Find Files',
+  -- },
+  {
+    '<leader>,',
+    function()
+      Snacks.picker.buffers()
+    end,
+    desc = 'Buffers',
+  },
+  {
+    '<leader>/',
+    function()
+      Snacks.picker.grep()
+    end,
+    desc = 'Grep',
+  },
+  {
+    '<leader>:',
+    function()
+      Snacks.picker.command_history()
+    end,
+    desc = 'Command History',
+  },
+  {
+    '<leader>n',
+    function()
+      Snacks.picker.notifications()
+    end,
+    desc = 'Notification History',
+  },
+  {
+    '<leader>e',
+    function()
+      Snacks.explorer()
+    end,
+    desc = 'File Explorer',
+  },
+  -- find
+  {
+    '<leader>fb',
+    function()
+      Snacks.picker.buffers()
+    end,
+    desc = 'Buffers',
+  },
+  {
+    '<leader>fp',
+    function()
+      Snacks.picker.files { cwd = vim.fn.stdpath 'config' }
+    end,
+    desc = 'Find Config File',
+  },
+  {
+    '<leader>fg',
+    function()
+      Snacks.picker.git_files()
+    end,
+    desc = 'Find Git Files',
+  },
+  {
+    '<leader>ff',
+    function()
+      Snacks.picker.recent()
+    end,
+    desc = 'Recent',
+  },
+  -- git
+  {
+    '<leader>gb',
+    function()
+      Snacks.picker.git_branches()
+    end,
+    desc = 'Git Branches',
+  },
+  {
+    '<leader>gl',
+    function()
+      Snacks.picker.git_log()
+    end,
+    desc = 'Git Log',
+  },
+  {
+    '<leader>gL',
+    function()
+      Snacks.picker.git_log_line()
+    end,
+    desc = 'Git Log Line',
+  },
+  {
+    '<leader>gs',
+    function()
+      Snacks.picker.git_status()
+    end,
+    desc = 'Git Status',
+  },
+  {
+    '<leader>gS',
+    function()
+      Snacks.picker.git_stash()
+    end,
+    desc = 'Git Stash',
+  },
+  {
+    '<leader>gd',
+    function()
+      Snacks.picker.git_diff()
+    end,
+    desc = 'Git Diff (Hunks)',
+  },
+  {
+    '<leader>gf',
+    function()
+      Snacks.picker.git_log_file()
+    end,
+    desc = 'Git Log File',
+  },
+  -- Grep
+  {
+    '<leader>sb',
+    function()
+      Snacks.picker.lines()
+    end,
+    desc = 'Buffer Lines',
+  },
+  {
+    '<leader>sB',
+    function()
+      Snacks.picker.grep_buffers()
+    end,
+    desc = 'Grep Open Buffers',
+  },
+  {
+    '<leader>sg',
+    function()
+      Snacks.picker.grep()
+    end,
+    desc = 'Grep',
+  },
+  {
+    '<leader>sw',
+    function()
+      Snacks.picker.grep_word()
+    end,
+    desc = 'Visual selection or word',
+    mode = { 'n', 'x' },
+  },
+  -- search
+  {
+    '<leader>s"',
+    function()
+      Snacks.picker.registers()
+    end,
+    desc = 'Registers',
+  },
+  {
+    '<leader>s/',
+    function()
+      Snacks.picker.search_history()
+    end,
+    desc = 'Search History',
+  },
+  {
+    '<leader>sa',
+    function()
+      Snacks.picker.autocmds()
+    end,
+    desc = 'Autocmds',
+  },
+  {
+    '<leader>sb',
+    function()
+      Snacks.picker.lines()
+    end,
+    desc = 'Buffer Lines',
+  },
+  {
+    '<leader>sc',
+    function()
+      Snacks.picker.commands()
+    end,
+    desc = 'Commands',
+  },
+  {
+    '<leader>sd',
+    function()
+      Snacks.picker.diagnostics()
+    end,
+    desc = 'Diagnostics',
+  },
+  {
+    '<leader>sD',
+    function()
+      Snacks.picker.diagnostics_buffer()
+    end,
+    desc = 'Buffer Diagnostics',
+  },
+  {
+    '<leader>sh',
+    function()
+      Snacks.picker.help()
+    end,
+    desc = 'Help Pages',
+  },
+  {
+    '<leader>sH',
+    function()
+      Snacks.picker.highlights()
+    end,
+    desc = 'Highlights',
+  },
+  {
+    '<leader>si',
+    function()
+      Snacks.picker.icons()
+    end,
+    desc = 'Icons',
+  },
+  {
+    '<leader>sj',
+    function()
+      Snacks.picker.jumps()
+    end,
+    desc = 'Jumps',
+  },
+  {
+    '<leader>sk',
+    function()
+      Snacks.picker.keymaps()
+    end,
+    desc = 'Keymaps',
+  },
+  {
+    '<leader>sl',
+    function()
+      Snacks.picker.loclist()
+    end,
+    desc = 'Location List',
+  },
+  {
+    '<leader>sm',
+    function()
+      Snacks.picker.marks()
+    end,
+    desc = 'Marks',
+  },
+  {
+    '<leader>sM',
+    function()
+      Snacks.picker.man()
+    end,
+    desc = 'Man Pages',
+  },
+  {
+    '<leader>sp',
+    function()
+      Snacks.picker.lazy()
+    end,
+    desc = 'Search for Plugin Spec',
+  },
+  {
+    '<leader>sq',
+    function()
+      Snacks.picker.qflist()
+    end,
+    desc = 'Quickfix List',
+  },
+  {
+    '<leader>sr',
+    function()
+      Snacks.picker.resume()
+    end,
+    desc = 'Resume',
+  },
+  {
+    '<leader>su',
+    function()
+      Snacks.picker.undo()
+    end,
+    desc = 'Undo History',
+  },
+  {
+    '<leader>sC',
+    function()
+      Snacks.picker.colorschemes()
+    end,
+    desc = 'Colorschemes',
+  },
+  {
+    '<leader>p',
+    function()
+      Snacks.picker.projects()
+    end,
+    desc = 'Projects',
+  },
+  {
+    '<leader>fl',
+    function()
+      Snacks.picker.lines()
+    end,
+    desc = 'Buffer Lines',
+  },
+  {
+    '<leader>N',
+    desc = 'Neovim News',
+    function()
+      Snacks.win {
+        file = vim.api.nvim_get_runtime_file('doc/news.txt', false)[1],
+        width = 0.6,
+        height = 0.6,
+        wo = {
+          spell = false,
+          wrap = false,
+          signcolumn = 'yes',
+          statuscolumn = ' ',
+          conceallevel = 3,
+        },
+      }
+    end,
+  },
+  {
+    'gd',
+    function()
+      Snacks.picker.lsp_definitions()
+    end,
+    desc = 'Goto Definition',
+  },
+  {
+    'gD',
+    function()
+      Snacks.picker.lsp_declarations()
+    end,
+    desc = 'Goto Declaration',
+  },
+  {
+    'gr',
+    function()
+      Snacks.picker.lsp_references()
+    end,
+    nowait = true,
+    desc = 'References',
+  },
+  {
+    'gI',
+    function()
+      Snacks.picker.lsp_implementations()
+    end,
+    desc = 'Goto Implementation',
+  },
+  {
+    'gy',
+    function()
+      Snacks.picker.lsp_type_definitions()
+    end,
+    desc = 'Goto T[y]pe Definition',
+  },
+  {
+    'gs',
+    function()
+      Snacks.picker.lsp_symbols()
+    end,
+    desc = 'LSP Symbols',
+  },
+  {
+    'gS',
+    function()
+      Snacks.picker.lsp_workspace_symbols()
+    end,
+    desc = 'LSP Workspace Symbols',
+  },
+}
+
+dashboard_config = {
+  enabled = true,
+  sections = {
+    {
+      pane = 1,
+      section = 'terminal',
+      cmd = 'colorscript -e square',
+      height = 5,
+      --padding = 1,
+    },
+    -- {
+    --   section = 'terminal',
+    --   cmd = 'chafa ~/Desktop/bw.jpg --format symbols --symbols vhalf --size 60x17 --stretch; sleep .1',
+    --   height = 17,
+    --   padding = 1,
+    -- },
+    -- { section = 'keys', gap = 1, padding = 1 },
+    { section = 'projects' },
+    {
+      pane = 2,
+      icon = ' ',
+      desc = 'Browse Repo',
+      padding = 1,
+      key = 'b',
+      action = function()
+        Snacks.gitbrowse()
+      end,
+    },
+    function()
+      local in_git = Snacks.git.get_root() ~= nil
+      local cmds = {
+        {
+          title = 'Notifications',
+          cmd = 'gh notify -s -a -n5',
+          action = function()
+            vim.ui.open 'https://github.com/notifications'
+          end,
+          key = 'n',
+          icon = ' ',
+          height = 5,
+          pane = 2,
+          enabled = true,
+        },
+        {
+          title = 'Open Issues',
+          cmd = 'gh issue list -L 3',
+          key = 'i',
+          action = function()
+            vim.fn.jobstart('gh issue list --web', { detach = true })
+          end,
+          icon = ' ',
+          height = 7,
+        },
+        {
+          icon = ' ',
+          title = 'Open PRs',
+          cmd = 'gh pr list -L 3',
+          key = 'P',
+          action = function()
+            vim.fn.jobstart('gh pr list --web', { detach = true })
+          end,
+          pane = 2,
+          height = 7,
+        },
+      }
+      return vim.tbl_map(function(cmd)
+        return vim.tbl_extend('force', {
+          section = 'terminal',
+          enabled = in_git,
+          padding = 2,
+          ttl = 5 * 60,
+          indent = 3,
+        }, cmd)
+      end, cmds)
+    end,
+  },
+}
+
 return {
   'folke/snacks.nvim',
   priority = 1000,
@@ -5,95 +461,8 @@ return {
   ---@type snacks.Config
   opts = {
     bigfile = { enabled = true },
-    dashboard = {
-      enabled = true,
-      sections = {
-        {
-          pane = 1,
-          section = 'terminal',
-          cmd = 'colorscript -e square',
-          height = 5,
-          --padding = 1,
-        },
-        -- {
-        --   section = 'terminal',
-        --   cmd = 'chafa ~/Desktop/bw.jpg --format symbols --symbols vhalf --size 60x17 --stretch; sleep .1',
-        --   height = 17,
-        --   padding = 1,
-        -- },
-        -- { section = 'keys', gap = 1, padding = 1 },
-        { section = 'projects' },
-        {
-          pane = 2,
-          icon = ' ',
-          desc = 'Browse Repo',
-          padding = 1,
-          key = 'b',
-          action = function()
-            Snacks.gitbrowse()
-          end,
-        },
-        function()
-          local in_git = Snacks.git.get_root() ~= nil
-          local cmds = {
-            {
-              title = 'Notifications',
-              cmd = 'gh notify -s -a -n5',
-              action = function()
-                vim.ui.open 'https://github.com/notifications'
-              end,
-              key = 'n',
-              icon = ' ',
-              height = 5,
-              pane = 2,
-              enabled = true,
-            },
-            {
-              title = 'Open Issues',
-              cmd = 'gh issue list -L 3',
-              key = 'i',
-              action = function()
-                vim.fn.jobstart('gh issue list --web', { detach = true })
-              end,
-              icon = ' ',
-              height = 7,
-            },
-            {
-              icon = ' ',
-              title = 'Open PRs',
-              cmd = 'gh pr list -L 3',
-              key = 'P',
-              action = function()
-                vim.fn.jobstart('gh pr list --web', { detach = true })
-              end,
-              pane = 2,
-              height = 7,
-            },
-          }
-          return vim.tbl_map(function(cmd)
-            return vim.tbl_extend('force', {
-              section = 'terminal',
-              enabled = in_git,
-              padding = 2,
-              ttl = 5 * 60,
-              indent = 3,
-            }, cmd)
-          end, cmds)
-        end,
-        --{ section = 'startup' },
-      },
-    },
-    -- explorer = { enabled = true },
-    -- indent = { enabled = true },
-    -- input = { enabled = true },
-    -- notifier = {
-    --   enabled = true,
-    --   timeout = 3000,
-    -- },
-    -- picker = { enabled = true },
-    -- quickfile = { enabled = true },
-    -- scope = { enabled = true },
-    -- scroll = { enabled = true },
+    picker = { enabled = true },
+    dashboard = dashboard_config,
     statuscolumn = { enabled = true },
     words = { enabled = true },
     styles = {
@@ -102,40 +471,7 @@ return {
       },
     },
   },
-  keys = {
-    {
-      '<leader>p',
-      function()
-        Snacks.picker.projects()
-      end,
-      desc = 'Projects',
-    },
-    {
-      '<leader>fl',
-      function()
-        Snacks.picker.lines()
-      end,
-      desc = 'Buffer Lines',
-    },
-    {
-      '<leader>N',
-      desc = 'Neovim News',
-      function()
-        Snacks.win {
-          file = vim.api.nvim_get_runtime_file('doc/news.txt', false)[1],
-          width = 0.6,
-          height = 0.6,
-          wo = {
-            spell = false,
-            wrap = false,
-            signcolumn = 'yes',
-            statuscolumn = ' ',
-            conceallevel = 3,
-          },
-        }
-      end,
-    },
-  },
+  keys = keymaps,
   init = function()
     vim.api.nvim_create_autocmd('User', {
       pattern = 'VeryLazy',
