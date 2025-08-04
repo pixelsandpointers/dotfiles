b7da3d404c081368f5b71ccde1ddade959fb3498
Benjamin Beilharz <b@ben.graphics>
2025-08-04 14:19:07 +0200

    Nvim: added FFF

---
+++ nvim/lua/plugins/fff.lua
@@ -0,0 +1,25 @@
+return {
+  'dmtrKovalenko/fff.nvim',
+  build = 'cargo build --release',
+  -- or if you are using nixos
+  -- build = "nix run .#release",
+  opts = {
+    prompt = '>> ',
+  },
+  keys = {
+    {
+      '<leader><space>', -- try it if you didn't it is a banger keybinding for a picker
+      function()
+        require('fff').find_files() -- or find_in_git_root() if you only want git files
+      end,
+      desc = 'Open file picker',
+    },
+    {
+      'ffo',
+      function()
+        require('fff').find_files_in_dir()
+      end,
+      desc = 'Find files in directory',
+    },
+  },
+}
