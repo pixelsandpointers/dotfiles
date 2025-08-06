e1a4f6e282a8ab17d435c134d625942611506f02
Benjamin Beilharz <b@ben.graphics>
2025-08-06 09:03:05 +0200

    Nvim: Added cpp bindings

--- nvim/ftplugin/cpp.lua
+++ nvim/ftplugin/cpp.lua
@@ -1,5 +1,7 @@
 local map = require('utils').map
+local clangx = require 'clangd_extensions'
 
+map('n', '<localleader>h', '<Cmd>ClangdSwitchSourceHeader<CR>', { desc = 'Switch to header' })
 map('n', '<localleader>rc', ':!cmake -S . -B build -DCMAKE_EXPORT_COMPILE_COMMANDS=1 && cmake --build build<CR>', { buffer = true, desc = 'CMake: Compile' })
 map('n', '<localleader>rb', ':!cmake --build build -j16', { desc = 'Build' })
 map('n', '<localleader>rr', function()
