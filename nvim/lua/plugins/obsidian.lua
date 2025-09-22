6a93a972ee93a602e8214a8cb76de0afb8d5ad2b
Benjamin Beilharz <b@ben.graphics>
2025-09-22 22:36:26 +0200

    Nvim: Makes Obsidian with Nvim work

--- nvim/lua/plugins/obsidian.lua
+++ nvim/lua/plugins/obsidian.lua
@@ -1,34 +1,86 @@
 return {
-  'obsidian-nvim/obsidian.nvim',
-  version = '*', -- recommended, use latest release instead of latest commit
-  lazy = true,
-  ft = 'markdown',
-  -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
-  -- event = {
-  --   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
-  --   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/*.md"
-  --   -- refer to `:h file-pattern` for more examples
-  --   "BufReadPre path/to/my-vault/*.md",
-  --   "BufNewFile path/to/my-vault/*.md",
-  -- },
-  dependencies = {
-    -- Required.
-    'nvim-lua/plenary.nvim',
+  {
+    'obsidian-nvim/obsidian.nvim',
+    version = '*', -- recommended, use latest release instead of latest commit
+    lazy = true,
+    ft = 'markdown',
+    -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
+    -- event = {
+    --   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
+    --   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/*.md"
+    --   -- refer to `:h file-pattern` for more examples
+    --   "BufReadPre path/to/my-vault/*.md",
+    --   "BufNewFile path/to/my-vault/*.md",
+    -- },
+    dependencies = {
+      -- Required.
+      'nvim-lua/plenary.nvim',
 
-    -- see below for full list of optional dependencies 👇
-  },
-  opts = {
-    workspaces = {
-      {
-        name = 'personal',
-        path = '~/Library/Mobile Documents/iCloud~md~obsidian/Documents/Vault',
-      },
-      {
-        name = 'labwiki',
-        path = '~/lab/perception_wiki/',
-      },
+      -- see below for full list of optional dependencies 👇
     },
-
-    -- see below for full list of options 👇
+    opts = {
+      workspaces = {
+        {
+          name = 'Vault',
+          path = '~/Library/Mobile Documents/iCloud~md~obsidian/Documents/Vault',
+        },
+        -- {
+        --   name = 'labwiki',
+        --   path = '~/lab/perception_wiki/',
+        -- },
+      },
+      templates = {
+        folder = '+ Templates',
+      },
+      daily_notes = {
+        folder = '+ Dailies',
+        date_format = '%Y-%m-%d-%a', -- e.g. 2025-09-22-Mon
+        alias_format = '%B %-d, %Y', -- e.g. September 22, 2025
+        template = '+ Templates/daily_obs.md',
+      },
+      legacy_commands = false,
+    },
+  },
+  {
+    '3rd/image.nvim',
+    opts = {
+      backend = 'kitty', -- or "ueberzug" or "sixel"
+      processor = 'magick_cli', -- or "magick_rock"
+      integrations = {
+        markdown = {
+          enabled = true,
+          clear_in_insert_mode = false,
+          download_remote_images = true,
+          only_render_image_at_cursor = true,
+          only_render_image_at_cursor_mode = 'popup', -- or "inline"
+          floating_windows = false, -- if true, images will be rendered in floating markdown windows
+          filetypes = { 'markdown', 'vimwiki' }, -- markdown extensions (ie. quarto) can go here
+        },
+        neorg = {
+          enabled = false,
+          filetypes = { 'norg' },
+        },
+        typst = {
+          enabled = false,
+          filetypes = { 'typst' },
+        },
+        html = {
+          enabled = false,
+        },
+        css = {
+          enabled = false,
+        },
+      },
+      max_width = nil,
+      max_height = nil,
+      max_width_window_percentage = nil,
+      max_height_window_percentage = 50,
+      scale_factor = 1.0,
+      window_overlap_clear_enabled = false, -- toggles images when windows are overlapped
+      window_overlap_clear_ft_ignore = { 'cmp_menu', 'cmp_docs', 'snacks_notif', 'scrollview', 'scrollview_sign' },
+      editor_only_render_when_focused = false, -- auto show/hide images when the editor gains/looses focus
+      tmux_show_only_in_active_window = true, -- auto show/hide images in the correct Tmux window (needs visual-activity off)
+      hijack_file_patterns = { '*.png', '*.jpg', '*.jpeg', '*.gif', '*.webp', '*.avif' }, -- render image files as images when opened
+    },
   },
 }
