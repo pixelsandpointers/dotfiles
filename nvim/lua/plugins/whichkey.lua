90fba5cb55d51c3371d3a8e7a8f722f573e95272
Benjamin Beilharz <b@ben.graphics>
2026-01-22 15:40:04 +0100

    Nvim: Adds Claude Code plugin for Neovim
    (Requires the CLI to be installed and in path)

--- nvim/lua/plugins/whichkey.lua
+++ nvim/lua/plugins/whichkey.lua
@@ -6,12 +6,13 @@ return {
       preset = 'helix',
       spec = {
         -- Document existing key chains
+        { '<leader>a', name = 'Claude' },
         { '<leader>z', name = 'ZenMode' },
         { '<leader>c', name = 'Code' },
         { '<leader>d', name = 'Debug' },
         { '<leader>f', group = 'Find' },
         { '<leader>g', group = 'Git' },
-        { '<leader>o', group = 'Task' },
+        { '<leader>o', group = 'Task Runner' },
         { '<localleader>', group = 'Mode' },
       },
     },
