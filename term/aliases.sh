109444e858aab784df966042966a3f1bc9e5b415
Benjamin Beilharz <b@ben.graphics>
2026-02-03 12:37:47 +0100

    Term: Update aliases

--- term/aliases.sh
+++ term/aliases.sh
@@ -18,6 +18,11 @@ alias cuda-select='export CUDA_VISIBLE_DEVICES=$(nvidia-smi --query-gpu=memory.f
 alias hud='MTL_HUD_ENABLED=1'
 alias hud-log='MTL_HUD_ENABLED=1 MTL_HUD_LOG_ENABLED=1 MTL_HUD_LOG_SHADER_ENABLED=1'
 alias y='yazi'
+alias open='xdg-open'
 #alias ns="nix-search-tv print | fzf --preview 'nix-search-tv preview {}' --scheme history"
 
-alias vo='cd "$HOME/Google Drive/My Drive/Vault" && nvim -c "set ft=markdown" -c "Obsidian today"'
+if [[ "$(uname)" == "Darwin" ]]; then
+    alias vo='cd "$HOME/Google Drive/My Drive/Vault" && nvim -c "set ft=markdown" -c "Obsidian today"'
+else
+    alias vo='cd "$HOME/gdrive/Vault" && nvim -c "set ft=markdown" -c "Obsidian today"'
+fi
