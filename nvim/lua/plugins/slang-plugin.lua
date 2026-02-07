109141b3ab12ed79632ea8aa509af45646bed2ea
Benjamin Beilharz <b@ben.graphics>
2026-02-07 14:07:33 +0100

    Include a local development module
    - This introduces a local development folder and the slang package that
      fixes the synthetic file issue when plugging it into the regular LSP
    by resolving the URI. This has to be done before the picker starts.
    - Also implements a simple search function to navigate the module
      mapping to gd/gr for go to definition/reference respectively.

---
+++ nvim/lua/plugins/slang-plugin.lua
@@ -0,0 +1,13 @@
+-- slang.nvim - local plugin
+return {
+  dir = vim.fn.stdpath('config') .. '/dev/slang.nvim',
+  dependencies = {
+    'neovim/nvim-lspconfig',
+    'nvim-treesitter/nvim-treesitter',
+    'stevearc/conform.nvim',
+  },
+  opts = {
+    auto_format = true,
+    inlay_hints = true,
+  },
+}
