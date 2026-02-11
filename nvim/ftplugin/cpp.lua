1d320f9a4ffbe58939ef27ed1470be42accc1b40
Benjamin Beilharz <b@ben.graphics>
2026-02-11 11:33:47 +0100

    Nvim: Clean config and refactor

--- nvim/ftplugin/cpp.lua
+++ nvim/ftplugin/cpp.lua
@@ -2,9 +2,10 @@ local map = require('utils').map
 local clangx = require 'clangd_extensions'
 
 map('n', '<localleader>h', '<Cmd>ClangdSwitchSourceHeader<CR>', { desc = 'Switch to header' })
-map('n', '<localleader>rc', ':!cmake -S . -B build -DCMAKE_EXPORT_COMPILE_COMMANDS=1 && cmake --build build<CR>', { buffer = true, desc = 'CMake: Compile' })
-map('n', '<localleader>rb', ':!cmake --build build -j16', { desc = 'Build' })
-map('n', '<localleader>rr', function()
+map('n', '<localleader>bd', ':!cmake_deb -S . -B build && cmake --build build<CR>', { buffer = true, desc = 'CMake: Compile Debug' })
+map('n', '<localleader>br', ':!cmake_rel -S . -B build && cmake --build build<CR>', { buffer = true, desc = 'CMake: Compile Release' })
+map('n', '<localleader>bb', ':!cmake --build build -j16', { desc = 'CMake: Build' })
+map('n', '<localleader>r', function()
   local prog = vim.fn.input('Path to executable: ', './build/a.out')
   vim.api.nvim_command(':!' .. prog)
-end, { buffer = false, desc = 'Run' })
+end, { buffer = false, desc = 'Run executable' })
