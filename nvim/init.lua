a2cbaccc8474904fc8b19df321f72ba9da7d821b
Benjamin Beilharz <b@ben.graphics>
2026-03-31 22:32:18 +0200

    This commit introduces conditional movement keymaps for colemak

--- nvim/init.lua
+++ nvim/init.lua
@@ -3,6 +3,7 @@
 require 'options'
 require 'keymap'
 require 'autocommands'
+require('colemak').setup()
 
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
