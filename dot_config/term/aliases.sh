8198bf8dc4788fefbf523d79ddd49ebba80baa93
Benjamin Beilharz <b@ben.graphics>
2026-06-08 08:02:49 +0200

    adds vpn and fuzzy file finder functions

--- dot_config/term/aliases.sh
+++ dot_config/term/aliases.sh
@@ -27,3 +27,45 @@ else
     alias vo='cd "$HOME/gdrive/Vault" && nvim -c "set ft=markdown" -c "Obsidian today"'
     alias open='xdg-open'
 fi
+
+vpn() {
+  local password totp_code
+  password=$(pass-cli item view --vault-name Personal --item-title "TU Darmstadt" --field password)
+  totp_code=$(pass-cli item totp --vault-name Personal --item-title "TU Darmstadt" --output human | awk '/^totp:/{print $2}')
+  printf '%s\n%s\n' "$password" "$totp_code" | sudo openconnect vpn.hrz.tu-darmstadt.de \
+    --user=bb77suko \
+    --authgroup=extern \
+    --useragent=AnyConnect \
+    --no-external-auth \
+    --passwd-on-stdin
+}
+
+ff() (
+  RELOAD='reload:rg --column --color=always --smart-case {q} || :'
+  fzf --disabled --ansi --multi \
+      --bind "start:$RELOAD" --bind "change:$RELOAD" \
+      --bind 'alt-a:select-all,alt-d:deselect-all,ctrl-/:toggle-preview' \
+      --delimiter : \
+      --preview 'bat --style=full --color=always --highlight-line {2} {1}' \
+      --preview-window '~4,+{2}+4/3,<80(up)' \
+      --query "$*"
+)
+
+# ripgrep->fzf->vim [QUERY]
+rfv() (
+  RELOAD='reload:rg --column --color=always --smart-case {q} || :'
+  OPENER='if [[ $FZF_SELECT_COUNT -eq 0 ]]; then
+            nvim {1} +{2}     # No selection. Open the current line in Vim.
+          else
+            nvim +cw -q {+f}  # Build quickfix list for the selected items.
+          fi'
+  fzf --disabled --ansi --multi \
+      --bind "start:$RELOAD" --bind "change:$RELOAD" \
+      --bind "enter:become:$OPENER" \
+      --bind "ctrl-o:execute:$OPENER" \
+      --bind 'alt-a:select-all,alt-d:deselect-all,ctrl-/:toggle-preview' \
+      --delimiter : \
+      --preview 'bat --style=full --color=always --highlight-line {2} {1}' \
+      --preview-window '~4,+{2}+4/3,<80(up)' \
+      --query "$*"
+)
